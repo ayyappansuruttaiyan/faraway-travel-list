5d78c17cdcefad150eda6a5807b1fc17dab26b32
ayyappansuruttaiyan <ayyappan.suruttaiyan@gmail.com>
2023-11-04 22:41:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import "./index.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
+export default function App() {
+  //here, we can pass empty array also. But, passing initials items array, instead empty
+  // here the state is lifted up from form components to app components [items, setitems]
+  const [items, setItems] = useState([...initialItems]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
+      <Stats totalItems={initialItems.length} />
+    </div>
+  );
+}
+
+function Logo({ items }) {
+  return <h1>Far Away</h1>;
+}
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    initialItems.push(newItem);
+    console.log(newItem);
+    onAddItems(newItem);
+    //after submit input values are resetting
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(e.target.value);
+        }}
+      >
+        {Array.from({ length: 100 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Type to search"
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+      <button>ADD</button>
+    </form>
+  );
+}
+
+function PackingList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description} {item.packed}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats({ totalItems }) {
+  return (
+    <footer className="stats">
+      <em>
+        You have {totalItems} items on your list, and you already packed X (x%).
+      </em>
+    </footer>
+  );
+}
